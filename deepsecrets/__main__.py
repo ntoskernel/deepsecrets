bdd3cdfa708a2278d563cf1f0c93ab324b2f1525
Nikolai Khechumov <khechumov@gmail.com>
2024-05-06 17:19:15 +0300

    freeze

--- deepsecrets/__main__.py
+++ deepsecrets/__main__.py
@@ -1,4 +1,8 @@
+from multiprocessing import freeze_support
 import sys
-
 from deepsecrets.cli import DeepSecretsCliTool
-sys.exit(DeepSecretsCliTool(sys.argv).start())
+
+if __name__ == '__main__':
+    freeze_support()
+    sys.exit(DeepSecretsCliTool(sys.argv).start())
+

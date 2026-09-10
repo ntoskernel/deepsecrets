217ca78f3ba6a63e40871ba6003a929204a6a1bb
Nikolai Khechumov <khechumov@gmail.com>
2026-09-10 22:41:58 +0300

    bump version

--- deepsecrets/config.py
+++ deepsecrets/config.py
@@ -11,7 +11,7 @@ from deepsecrets.core.utils.fs import get_abspath, path_exists
 FALLBACK_PROCESS_COUNT = 4
 
 SCANNER_NAME = "DeepSecrets"
-SCANNER_VERSION = "2.0.1"
+SCANNER_VERSION = "2.1.0"
 SCANNER_VERSION_NUMERIC = [int(subver) for subver in SCANNER_VERSION.split('.')]
 SCANNER_URL = "https://github.com/ntoskernel/deepsecrets"
 
